6cc6f63195d25987cf8baf95cd9d9bfbd606c122
LefayJessy <144626770+LefayJessy@users.noreply.github.com>
2024-02-29 11:31:13 +0100

    Delete TP-6 directory

--- TP-6/afficherNbPassagers.php
+++
@@ -1,7 +0,0 @@
-<?php
-$nbPassagers = array(35, 10, 22, 52, 15, 58, 8, 73);
-
-foreach ($nbPassagers as $passager) {
-    echo $passager . " passagers\n";
-}
-?>
--- TP-6/rechercherPour52Passagers.php
+++
@@ -1,8 +0,0 @@
-<?php
-$nbPassagers = array( 35,10,22,52,15,58,8,73) ;
- 
- for ($i = 0 ; $i < count($nbPassagers); $i = $i + 1 ){
-	 
-}	
-		echo "La traversée 4 a transporté ",$nbPassagers[3]," passagers.\n";
-?>
